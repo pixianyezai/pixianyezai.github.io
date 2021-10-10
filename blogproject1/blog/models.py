31b3ab9f20fe2907bcbf3e9a4ab363524c0b9471
Drizzy <mirage0731@gmail.com>
2021-10-10 23:26:28 +0200

    modi

---
+++ blogproject1/blog/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils import timezone
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField('标题', max_length=70)
+    def __str__(self):
+        return self.body
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk':self.pk})
+
+
+    body = models.TextField('正文')
+
+    created_time = models.DateTimeField('创建时间', default=timezone.now)
+    modified_time = models.DateTimeField('修改时间')
+
+    excerpt = models.CharField( max_length=200, blank=True)
+
+    category = models.ForeignKey(Category,verbose_name='分类', on_delete=models.CASCADE) # 分类被删除那么旗下数据也被删除
+    tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
+
+
+    author=models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+        ordering = ['-created_time']
+
+        def  __str__(self):
+            return self.title
+    def save(self, *args, **kwargs):
+        self.modified_time = timezone.now()
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+        ])
+        self.excerpt = strip_tags(md.convert(self.body))[:54]
+
+        super().save(*args, **kwargs)
